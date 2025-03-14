0cea97b02bbf66144a8e7ae99149253bf1ad71cb
Matheus Silveira <matheus.silveiraw@gmail.com>
2025-03-14 16:33:13 -0300

    mvp na página inicial do projeto + configurações + model todo

--- src/main/java/com/todolistjava/to_do_list/ToDoListApplication.java
+++ src/main/java/com/todolistjava/to_do_list/ToDoListApplication.java
@@ -9,5 +9,8 @@ public class ToDoListApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoListApplication.class, args);
 	}
-
 }
+
+//mvp da minha to do list:
+//inserir, atualizar, deletar e buscar to dos
+//inicialmente a to do vai ter: nome, descrição, status (para finalziar a to do)
---
+++ src/main/java/com/todolistjava/to_do_list/models/ToDo.java
@@ -0,0 +1,54 @@
+package com.todolistjava.to_do_list.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "todos")
+public class ToDo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+    @Column(nullable = false, length = 300)
+    private String descricao;
+
+    @Column(nullable = false)
+    private Boolean status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+}
