353b86c1b8a1afe6440b8da8534491254919e8ca
Matheus Silveira <matheus.silveiraw@gmail.com>
2025-03-15 17:40:48 -0300

    get todos funcionando

--- src/main/java/com/todolistjava/to_do_list/controllers/TodoController.java
+++ src/main/java/com/todolistjava/to_do_list/controllers/TodoController.java
@@ -1,13 +1,43 @@
 package com.todolistjava.to_do_list.controllers;
 
+import com.todolistjava.to_do_list.models.ToDo;
 import com.todolistjava.to_do_list.services.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/todos")
 public class TodoController {
     @Autowired
     private TodoService todoService;
+
+    @GetMapping
+    public ResponseEntity<?> listarUsuarios() {
+        try {
+            List<ToDo> todos = todoService.listarTodos();
+
+            if (todos.isEmpty()) {
+            }
+
+            if (todos.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+                //retornando 204 (funcionou mas tá vazio, por padrão o back não pode retornar nada, tratar no front)
+            }
+
+            return ResponseEntity.ok(todos);
+        } catch (Exception e) {
+            Map<String, Object> response = new HashMap<>();
+            response.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+            response.put("message", "Erro ao listar usuários: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
+    }
 }
--- src/main/java/com/todolistjava/to_do_list/services/TodoService.java
+++ src/main/java/com/todolistjava/to_do_list/services/TodoService.java
@@ -1,12 +1,19 @@
 package com.todolistjava.to_do_list.services;
 
 
+import com.todolistjava.to_do_list.models.ToDo;
 import com.todolistjava.to_do_list.repositories.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TodoService {
     @Autowired
     private TodoRepository todoRepository;
+
+    public List<ToDo> listarTodos() {
+        return todoRepository.findAll();
+    }
 }
